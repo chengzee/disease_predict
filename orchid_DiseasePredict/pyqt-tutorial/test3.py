88e4b0f5dd1cf5110d246552ba572a865d0c5d2e
chengzee <as0780962@gmail.com>
2021-03-08 16:06:40 +0800

    update test3 for testing the prediction in GUI

--- orchid_DiseasePredict/pyqt-tutorial/test3.py
+++ orchid_DiseasePredict/pyqt-tutorial/test3.py
@@ -8,7 +8,7 @@ import time
 import cv2
 import pandas as pd
 import numpy as np
-# from MA_luongAtt_byCZ import * 
+from MA_luongAtt_byCZ import * 
 
 import requests
 url = "https://monitor.icmems.ml/api/getDatas"
@@ -17,7 +17,8 @@ temp = [[], [], [], [], [], [], [], [], [], [], [], []]
 humid = [[], [], [], [], [], [], [], [], [], [], [], []]
 light = [[], [], [], [], [], [], [], [], [], [], [], []]
 timestamp = [[], [], [], [], [], [], [], [], [], [], [], []]
-# print(len(temp))
+predictions= [[], [], [], [], [], [], [], [], [], [], [], []]
+
 for fs in range(12):
     dataUrl = url + "/" + str(fs+1)
     print(dataUrl)
@@ -34,6 +35,7 @@ for fs in range(12):
     timestamp_arr = np.array(timestamp[fs]).reshape(-1, 1)
     # 將各特徵結合至新矩陣後，進行資料前處理，才套用模型
     rawdata_array = np.append(np.append(np.append(timestamp_arr, humid_arr, axis=1), temp_arr, axis=1), light_arr, axis=1)
+    total_LossNumber = 0
     for n in range(len(rawdata_array)):
         if n > 0:
             # 計算時間差 (轉換為 unix timestamp)
@@ -56,3 +58,36 @@ for fs in range(12):
                     padding_array[v:v+1, 1:4] = rawdata_array[n-1:n, 1:4] + LossValue*(v+1)
                 paddeddata_array = np.vstack((np.vstack((paddeddata_array[:n+total_LossNumber], padding_array)), paddeddata_array[n+total_LossNumber:]))    
                 total_LossNumber += LossNumber
+    # model functional
+    input_seq = tf.keras.Input(shape=(_lookback, source_dim))
+    encoder_stack_h, enc_last_h = Encoder(neuron, A, _lookback, _source_dim)(input_seq)
+    decoder_input = tf.keras.layers.RepeatVector(_delay)(enc_last_h)
+    decoder_stack_h = Decoder(neuron, A)(decoder_input)
+    # attention layer
+    attention = tf.keras.layers.dot([decoder_stack_h, encoder_stack_h], axes=[2, 2])
+    attention = tf.keras.layers.Activation('softmax')(attention)
+    # print(attention)
+    context = tf.keras.layers.dot([attention, encoder_stack_h], axes=[2, 1])
+    # print(context)
+    decoder_combine_context = tf.keras.layers.concatenate([context, decoder_stack_h])
+    # print(decoder_combine_context)
+    out = tf.keras.layers.TimeDistributed(tf.keras.layers.Dense(_predict_dim))(decoder_combine_context)
+    model = tf.keras.models.Model(inputs=input_seq, outputs=out)
+
+    model.compile(optimizer='adam', loss='mse', metrics=['mae'])
+    model.summary()
+    # checkpoint
+    filepath = save_file_path + "/weights.best.h5"
+    checkpoint = tf.keras.callbacks.ModelCheckpoint(filepath=filepath, 
+                                                    monitor='val_loss', 
+                                                    verbose=1, 
+                                                    save_best_only=True,
+                                                    # save_weights_only=True, 
+                                                    mode='min')
+    callbacks_list = [checkpoint]
+
+    model.load_weights(filepath)
+
+    predictions[fs] = model.predict(paddeddata_array[_lookback:], verbose=1, batch_size=BATCH_SIZE)
+
+# make predict thermal plane
